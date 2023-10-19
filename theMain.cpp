a7cbc8a79c207c5c68a4766f1f165f6f3e55c67e
y.sato <y.sato@jc-21.jp>
2023-10-19 15:39:09 +0900

    ベクターを追加したよ。

--- theMain.cpp
+++ theMain.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	std::vector<int> v{ 1,2,3,4,5 };
 	std::cout << "hello, world" << std::endl;
 
+	for (auto& theI : v)
+		std::cout << theI << std::endl;
 	return 0;
 }
