c61c48a0a50b18bd2e34f143cb1d8e989a45d2a6
y.sato <y.sato@jc-21.jp>
2023-10-19 15:45:10 +0900

    構造体と、関数のプロトタイプ宣言を追加

--- theMain.cpp
+++ theMain.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
 
+//これは構造体の宣言
+struct abc
+{
+	int a, b, c;
+};
+
+//これはプロトタイプ宣言
+void maxabc(int _a, int _b, int _c);
+
 int main()
 {
 	std::vector<int> v{ 1,2,3,4,5 };
